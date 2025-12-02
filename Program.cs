847f3c32e988ca84249740de3ca98b032be1ffc9
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2025-12-02 16:26:42 -0500

    fixed communication between projects

--- Program.cs
+++ Program.cs
@@ -5,13 +5,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PickupExpressApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // HttpClient pointing to your API
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5070/") // matches API HTTP profile
+    BaseAddress = new Uri("http://localhost:5000/") // matches API HTTP profile
 });
 
 
