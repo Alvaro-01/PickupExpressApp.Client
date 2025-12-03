04049886f54878137f1e987d171def505ae36ee7
Jeremias Oliveira <olivjere@shernet.sheridancollege.ca>
2025-12-03 11:03:16 -0500

    fixed gitignore

---
+++ Services/IProductService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PickupExpressApp.Client.Models;
+
+namespace PickupExpressApp.Client.Services
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllProductsAsync();
+        Task<List<Product>> GetAvailableProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+
+        // Employee Operations
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<Product> UpdateQuantityInStockAsync(int id, int newQuantity);
+        Task<bool> DeleteProductAsync(int id);
+    }
+}
